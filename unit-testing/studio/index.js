c383d3096fdb2bd8fcc4e8c6eaf779d13ebae003
E J <johnsonelijah440@gmail.com>
2023-10-10 22:42:50 -0500

    <UnitTestStudio>

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,59 @@
 
 let launchcode = {
-  
-}
+  organization : "nonprofit",
+  executiveDierector : "Jeff",
+  percentageCoolEmployees : 100,
+  programsOffered : ["Web Development" , "Data Analysis", "Liftoff"],
+  launchOutput : function (number)
+  {
+    number = Number(number);
 
+    if(number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0)
+    {
+        return "Rutabagas! That doesn't work."
+    }
+
+    if(number % 2 === 0 )
+    {
+        if(number % 3 === 0)
+        {
+            if(number % 5 === 0)
+            {
+                return "LaunchCode Rocks!";
+            }
+            else
+            {
+                return "LaunchCode!";
+            }
+        }
+
+        else if (number % 5 === 0)
+        {
+            return "Launch Rocks! (CRASH!!!!)";
+        }
+
+        else
+        {
+            return "Launch!";
+        }
+    }
+    else if(number % 3 === 0 )
+    {
+        if(number % 5 === 0)
+        {
+            return "Code Rocks!";
+        }
+        else
+        {
+            return "Code!"
+        }
+    }
+    else if(number % 5 === 0 )
+    {
+        return "Rocks!";
+    }
+  }
+}
+console.log(launchcode.launchOutput(15));
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,72 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("Checking Organization", () =>
+  {
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
 
+  test("Checking Director", () =>
+  {
+    expect(launchcode.executiveDierector).toEqual("Jeff");
+  });
+
+  test("Checking Percentage of cool employees", () =>
+  {
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("Checking Programs Offered", () =>
+  {
+    expect(launchcode.programsOffered.length).toEqual(3);
+    expect(launchcode.programsOffered[0]).toEqual("Web Development");
+    expect(launchcode.programsOffered[1]).toEqual("Data Analysis");
+    expect(launchcode.programsOffered[2]).toEqual("Liftoff");
+
+  });
+
+  test("Checking for correct output", () =>
+  {
+    expect(typeof(launchcode.launchOutput('hi'))).toBe("string");
+  });
   // Write your unit tests here!
+  test("Checking for correct output of 2", () =>
+  {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+
+  test("Checking for correct output of 3", () =>
+  {
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+
+  test("Checking for correct output of 5", () =>
+  {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
   
+  test("Checking for correct output of 2 & 3", () =>
+  {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+
+  test("Checking for correct output of 2 & 5", () =>
+  {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+  });
+
+  test("Checking for correct output of 3 & 5", () =>
+  {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+
+  test("Checking for correct output of 2, 3, & 5", () =>
+  {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test("Checking for incorrect output of 2, 3, & 5", () =>
+  {
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+  });
 });
