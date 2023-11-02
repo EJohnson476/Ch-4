4cc9b59e7a3dacd539911a90de9ec0f700d807fa
E J <johnsonelijah440@gmail.com>
2023-11-02 15:42:05 -0500

    <Exercise>

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,19 @@
 //Code Your Solution Below
+window.addEventListener("load", function()
+{
+    let form = document.querySelector('form');
+    form.addEventListener('submit', function(event)
+    {
+        let var1 = document.querySelect("input[name=testName]");
+        let var2 = document.querySelect("date[name=testDate]");
+        let var3 = document.querySelect("select[name=rocketType]");
+        let var4 = document.querySelect("number[name=boosterCount]");
+        let var5 = document.querySelect("radio[name=windRating]");
+        let var6 = document.querySelect("checkbox[name=productionServers]");
+        if(var1.value === "" || var2.value === "" || var3.value === "" || var4.value === "" || var5.value === "" || var6.value === "on" ||)
+        {
+            alert("You must fill all options.")
+            event.preventDefault();
+        }
+    });
+});
